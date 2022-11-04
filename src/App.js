055e107179be290eb03a6fbb3f85cc9d947c06ad
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-11-04 20:54:10 +0300

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div className="App">
-      <h2></h2>
+      <AppRouter />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <div className="d-flex align-items-center">
+            <h5 className="mb-0">Ismail</h5>
+            <button className="ms-2 btn btn-outline-dark">Logout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
