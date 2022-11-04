5b4ba6981e581ea6fb9660af95e810cffb55a121
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-11-04 21:05:41 +0300

    navbar finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,39 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  //   const currentUser = { displayName: "Mark Madison" };
+  const currentUser = false;
   return (
     <div>
       <div className="navbar navbar-expand-lg">
         <div className="container">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
+          <Link className="navbar-brand" to="/">
+            <h3>Ninjas Movie App</h3>
+          </Link>
           <div className="d-flex align-items-center">
-            <h5 className="mb-0">Ismail</h5>
-            <button className="ms-2 btn btn-outline-dark">Logout</button>
+            {currentUser ? (
+              <>
+                <h5 className="mb-0">{currentUser.displayName}</h5>
+                <button className="ms-2 btn btn-outline-dark">Logout</button>
+              </>
+            ) : (
+              <>
+                <button
+                  onClick={() => navigate("/login")}
+                  className="ms-2 btn btn-outline-light"
+                >
+                  Login
+                </button>
+                <button
+                  onClick={() => navigate("/register")}
+                  className="ms-2 btn btn-outline-light"
+                >
+                  Register
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
